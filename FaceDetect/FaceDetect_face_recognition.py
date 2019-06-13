6ebb2d966ea10f67fb0eef29246258b85b03d039
RoWe98 <wzx8551517@163.com>
2019-06-13 15:10:57 +0800

    添加了face_recognition引用

---
+++ FaceDetect/FaceDetect_face_recognition.py
@@ -0,0 +1,28 @@
+
+import cv2
+import sys
+
+import face_recognition
+
+# Get user supplied values
+imagePath = sys.argv[1]
+
+# Load the image from face_recognition
+image = face_recognition.load_image_file(imagePath)
+
+# Detect faces in image
+
+face_loaction = face_recognition.face_locations(image)
+
+print("Found {0} Faces!".format(len(face_loaction)))
+
+# Read the image with openCV
+
+image = cv2.imread(imagePath)
+
+# Draw a rectangle around the faces
+for(top, right, bottom, left) in face_loaction:
+    cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", cv2.resize(image,(1024,768)))
+cv2.waitKey(0)
